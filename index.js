bbf70820e273a7656a4c979e2e6e16a66dcf158c
Adam Richman <adamsrichman1@gmail.com>
2019-04-04 19:38:26 -0400

    finished endpoints

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * TODO - swagger-doc for API, finish two post-endpoints, finish survey_engine, unit test, integration test, README
+ * TODO - swagger-doc for API, finish survey_engine, unit test, integration test, README
  */
 import express from "express";
 
@@ -32,7 +32,7 @@ app.get("/get-survey", (req, res) => {
 /**
  * GET endpoint to request survey results - accepts 2 query parameters
  * id: String to identify the survey
- * username: the id of the user who took the survey
+ * username: String id of the user who took the survey
  *
  * @param req the HTTP request object
  * @param res the HTTP response object
@@ -43,7 +43,7 @@ app.get("/get-survey-results", (req, res) => {
         if (survey_engine.user_has_taken_survey(req.query.id, req.query.username)) {
             return res.send(survey_engine.get_survey_results(req.query.id, req.query.username), res.ok);
         }
-        res.send("User with username " + req.query.username + " has not taken the specified survey", res.statusCode=400);
+        return res.send("User with username " + req.query.username + " has not taken the specified survey", res.statusCode=400);
     }
     res.send("Survey id not recognized: " + req.query.id, res.statusCode=400);
 });
@@ -52,6 +52,7 @@ app.get("/get-survey-results", (req, res) => {
 /**
  * POST endpoint to create a survey - accepts 1 query parameter
  * id: String to identify the survey
+ * Body: contains the survey questions as a JSON array of Strings
  *
  * @param req the HTTP request object
  * @param res the HTTP response object
@@ -59,27 +60,37 @@ app.get("/get-survey-results", (req, res) => {
  * */
 app.post("/create-survey", (req, res) => {
     if (!survey_engine.survey_exists(req.query.id)) {
-        survey_engine.create_survey(req.query.id);
-        return res.send(res.ok);
+        if (survey_engine.survey_valid(req.query.id, req.body)) {
+            survey_engine.add_survey(req.query.id, req.body);
+            return res.send(res.ok);
+        }
+        res.statusCode=400;
+        return res.send("Survey questions were not valid.");
     }
     res.statusCode=400;
-    res.send("Survey with the specified id has already been created!");
+    res.send("Sorry! Survey with the specified id has already been created.");
 });
 
 
 /**
  * POST endpoint to submit survey results - accepts 1 query parameter
  * id: String to identify the survey
+ * username: String id of the user who took the survey
+ * Body: contains the survey answers as a JSON array of Booleans
  *
  * @param req the HTTP request object
  * @param res the HTTP response object
- * @return 200 on success, 400 bad request if a survey with the specified id has already been created
+ * @return 200 on success, 400 bad request if the survey id is invalid
  * */
 app.post("/submit-survey", (req, res) => {
-    if (!survey_engine.survey_exists(req.query.id)) {
-        survey_engine.create_survey(req.query.id);
-        return res.send(res.ok);
+    if (survey_engine.survey_exists(req.query.id)) {
+        if (survey_engine.survey_results_valid(req.query.id, req.body)) {
+            survey_engine.save_survey_results(req.query.id, req.query.username, req.body);
+            return res.send(res.ok);
+        }
+        res.statusCode = 400;
+        return res.send("Survey responses invalid!");
     }
     res.statusCode=400;
-    res.send("Survey with the specified id has already been created!");
+    res.send("Survey with the specified id does not exist!");
 });
--- survey_engine.js
+++ survey_engine.js
@@ -7,7 +7,7 @@ class survey_engine {
      * @returns {JSON} the survey in JSON format
      */
     static get_survey(survey_id) {
-
+        // TODO
     }
 
     /**
@@ -17,25 +17,38 @@ class survey_engine {
      * @returns {JSON} the user's results from the survey in JSON format
      */
     static get_survey_results(survey_id, username) {
-
+        // TODO
     }
 
     /**
+     * Adds a new survey to the system
      *
-     * @param survey_id
-     * @returns {boolean}
+     * @param survey_id the id of the survey to add
+     * @param survey_questions the questions of the survey
      */
-    static create_survey(survey_id) {
-
+    static add_survey(survey_id, survey_questions) {
+        // TODO
     }
 
     /**
+     * Checks if a survey with a given id exists
      *
      * @param survey_id the id of the survey to check
-     * @return {boolean} true if the survey exists, false otherwise
+     * @returns {boolean} true if the survey exists, false otherwise
      */
     static survey_exists(survey_id) {
+        // TODO
+    }
 
+    /**
+     * Saves results for a survey
+     *
+     * @param survey_id the id of the survey to associate the survey results with
+     * @param username the id of the user to associate the survey results with
+     * @param survey_results the results of the survey
+     */
+    static save_survey_results(survey_id, username, survey_results) {
+        // TODO
     }
 
     /**
@@ -43,9 +56,20 @@ class survey_engine {
      *
      * @param survey_id the id of the survey to check
      * @param username the id of the user to check
-     * @return {boolean} true if the user has taken the specified survey, false otherwise
+     * @returns {boolean} true if the user has taken the specified survey, false otherwise
      */
     static user_has_taken_survey(survey_id, username) {
+        // TODO
+    }
 
+    /**
+     * Checks if responses to a survey are valid before saving them
+     *
+     * @param survey_id the id of the survey to check
+     * @param survey_results the results of the survey
+     * @return {boolean} true if the results of the survey are valid, false otherwise
+     */
+    static survey_results_valid(survey_id, survey_results) {
+        // TODO
     }
 }
